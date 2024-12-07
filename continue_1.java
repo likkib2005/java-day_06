872c4b7222645d90db6013658b1a3cd00bd10a42
likkib2005 <likkilikitha214@gmail.com>
2024-12-07 11:47:28 +0530

    Add files via upload

---
+++ continue_1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class continue_1
+{
+    public static void main(String args[])
+    {
+        System.out.println("break example");
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            continue;
+            System.out.println(i);
+        }
+    }
+}
+    
